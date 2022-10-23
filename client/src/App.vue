79e7d00f3295f416f212285896f95154d4972237
Sanjna <sanjna021983@gmail.com>
2022-10-23 21:55:35 +0530

    ASD

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,33 @@
 <template>
+    <Timer :id="'timer_txt'" />
+    <form>
+        <input type="text" v-model="name"/>
+        <select name="students" v-model="selectedStudents" size="10" multiple>
+            <option v-for="(name, index) in students" :key="name" :value="name">{{index+1}} --> {{name}}</option>
+        </select>
+    </form>
+    {{ name}}
+    <div v-html="channel" v-bind:id="'timer_txt2'"></div>
+    <button :disabled="isDisabled" @click="disableEnable" >Click Me</button>
+    <button @click="suffle" v-on:mouseover="show($event)" >Click Me</button>
+    <div :style="{
+        color:'red',
+        'font-weight':'bold',
+    }">{{ name }}</div>
+    <ul>
+        <li v-for="(name, index) in students" :key="name">{{index+1}} --> {{name}}</li>
+    </ul>
+    <ul>
+        <li v-for="(name, index) in fullNames" :key="name.first">{{index+1}} --> {{name.first}} {{name.last}}</li>
+    </ul>
+    <ul>
+        <template v-for="(value, name, index ) in myinfo" :key="name.first">
+            <li v-if="index%2==0">
+                <label>{{index+1}} --> {{name}} {{value}}</label>
+                <input type="text" :value="value" :id="name" :name="name" @change="alertThis($event)"/>
+            </li>
+        </template>
+    </ul>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -28,3 +57,47 @@ nav {
   }
 }
 </style>
+<script>
+import Timer from "@/components/Timer";
+import _ from "lodash";
+export default {
+    name:"App",
+    data() {
+        return {
+            name: "Vipan Kumar",
+            channel: '<b>code</b>',
+            isDisabled: false,
+            students: ["vipan", "kumar",'karan'],
+            selectedStudents:["vipan", "kumar",'karan'],
+            fullNames: [
+                { first: "karan", last: "Kumar"},
+                { first: "karan", last: "Kumar"},
+                { first: "Diana",last: "Princes"}
+            ],
+            myinfo:{
+                name: "Vipan Kumar",
+                age: "918",
+                address: "Address"
+            }
+        }
+    },
+    methods: {
+        alertThis(event){
+            console.log(event)
+          alert('aaa')
+        },
+        disableEnable(){
+          this.isDisabled = !this.isDisabled;
+        },
+        show(event){
+            console.log(event)
+        },
+        suffle(){
+            this.students = _.shuffle(this.students);
+            this.fullNames = _.shuffle(this.fullNames);
+            this.myinfo = _.shuffle(this.myinfo);
+        }
+    },
+    components: {Timer}
+}
+</script>
---
+++ client/src/components/Timer.vue
@@ -0,0 +1,20 @@
+<template>
+    <form>
+        <div>
+            <input type="text"/>
+        </div>
+    </form>
+</template>
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+export default class Timer extends Vue{
+    data(){
+        return {
+            inputLine: ""
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
