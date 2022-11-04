d2a0e1c760622d12be4c00ccde811c124a3a9ad4
Sanjna <sanjna021983@gmail.com>
2022-11-04 14:30:38 +0530

    Wa

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,11 +1,78 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+    >
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+        title="John Leider"
+      ></v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+
+    <v-app-bar :elevation="0" rounded color="primary">
+      <template v-slot:image>
+        <v-img
+        ></v-img>
+      </template>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Todo List</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <template v-slot:append>
+        <v-btn icon="mdi-heart"></v-btn>
+
+        <v-btn icon="mdi-magnify"></v-btn>
+
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
     <v-main>
-      <HelloWorld />
+      <v-container fluid>
+      </v-container>
     </v-main>
+    <v-footer class="bg-grey-lighten-1">
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Tiny Todo</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
-<script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+<script>
+export default {
+  data () {
+    return {
+      drawer: null,
+    }
+  },
+}
 </script>
